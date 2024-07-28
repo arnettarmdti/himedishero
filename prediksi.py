9d7524be0611c0ea4a7485c9bc729b3aeecf058d
arnettarmdti <arnettarmdti27@gmail.com>
2024-07-28 22:04:19 +0700

    Add Firebase credentials and Streamlit setup

--- prediksi.py
+++ prediksi.py
@@ -6,24 +6,25 @@ import joblib
 import firebase_admin
 from firebase_admin import credentials, db
 
+# Inisialisasi aplikasi Flask
 app = Flask(__name__)
 
-# Load environment variables
-firebase_creds = {
-    "type": os.getenv("FIREBASE_TYPE"),
-    "project_id": os.getenv("FIREBASE_PROJECT_ID"),
-    "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
-    "private_key": os.getenv("FIREBASE_PRIVATE_KEY", "").replace('\\n', '\n'),
-    "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
-    "client_id": os.getenv("FIREBASE_CLIENT_ID"),
-    "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
-    "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
-    "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_X509_CERT_URL"),
-    "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_X509_CERT_URL")
-}
+# Memuat model XGBoost yang sudah dilatih
+model = joblib.load('Flask Server/xgboost_model3.pkl')
 
-if not all(firebase_creds.values()):
-    raise ValueError("One or more Firebase credentials environment variables are not set.")
+# Mengonfigurasi Firebase
+firebase_creds = {
+    "type": os.environ.get("FIREBASE_TYPE"),
+    "project_id": os.environ.get("FIREBASE_PROJECT_ID"),
+    "private_key_id": os.environ.get("FIREBASE_PRIVATE_KEY_ID"),
+    "private_key": os.environ.get("FIREBASE_PRIVATE_KEY").replace('\\n', '\n'),
+    "client_email": os.environ.get("FIREBASE_CLIENT_EMAIL"),
+    "client_id": os.environ.get("FIREBASE_CLIENT_ID"),
+    "auth_uri": os.environ.get("FIREBASE_AUTH_URI"),
+    "token_uri": os.environ.get("FIREBASE_TOKEN_URI"),
+    "auth_provider_x509_cert_url": os.environ.get("FIREBASE_AUTH_PROVIDER_X509_CERT_URL"),
+    "client_x509_cert_url": os.environ.get("FIREBASE_CLIENT_X509_CERT_URL")
+}
 
 cred = credentials.Certificate(firebase_creds)
 firebase_admin.initialize_app(cred, {
@@ -66,4 +67,4 @@ def predict():
         return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=5000)
