86b51bcd85a34094b1e02a45eb33000661526d7b
arnettarmdti <arnettarmdti27@gmail.com>
2024-07-28 20:32:19 +0700

    Update konfigurasi untuk Vercel

--- prediksi.py
+++ prediksi.py
@@ -1,24 +1,19 @@
+import os
 from flask import Flask, request, jsonify
 import xgboost as xgb
 import numpy as np
 import joblib
 import firebase_admin
 from firebase_admin import credentials, db
-import os
 
-# Inisialisasi aplikasi Flask
 app = Flask(__name__)
 
-# Memuat model XGBoost yang sudah dilatih
-model_path = os.path.join(os.path.dirname(__file__), 'xgboost_model3.pkl')
-model = joblib.load(model_path)
-
-# Mengonfigurasi Firebase
+# Load environment variables
 firebase_creds = {
     "type": os.getenv("FIREBASE_TYPE"),
     "project_id": os.getenv("FIREBASE_PROJECT_ID"),
     "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
-    "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace('\\n', '\n'),
+    "private_key": os.getenv("FIREBASE_PRIVATE_KEY", "").replace('\\n', '\n'),
     "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
     "client_id": os.getenv("FIREBASE_CLIENT_ID"),
     "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
@@ -26,6 +21,9 @@ firebase_creds = {
     "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_X509_CERT_URL"),
     "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_X509_CERT_URL")
 }
+
+if not all(firebase_creds.values()):
+    raise ValueError("One or more Firebase credentials environment variables are not set.")
 
 cred = credentials.Certificate(firebase_creds)
 firebase_admin.initialize_app(cred, {
@@ -68,4 +66,4 @@ def predict():
         return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
